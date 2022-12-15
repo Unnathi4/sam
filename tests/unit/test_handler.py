c1e6480d0a8dc45b2ecd8da8c063717ad377e4d4
Unnathi4 <119917958+Unnathi4@users.noreply.github.com>
2022-12-15 22:45:52 +0530

    Update test_handler.py

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -22,13 +22,13 @@ def quotient():
 class TestSum(unittest.TestCase):
 
     def test_add(self):
-        self.assertEqual(app.sum(2,2), 4)
+        self.assertEqual(sum(2,2), 4)
     def test_product(self):
-        self.assertEqual(app.product(3, 2), 6)
+        self.assertEqual(product(3, 2), 6)
     def test_sub(self):
-        self.assertEqual(app.difference(4, 1), 3)
+        self.assertEqual(difference(4, 1), 3)
     def test_division(self):
-        self.assertEqual(app.quotient(4,2), 2)
+        self.assertEqual(quotient(4,2), 2)
 
 if __name__ == '__main__':
     unittest.main()
