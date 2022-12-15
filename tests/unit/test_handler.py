8fa6076887a8e0334db0982fdb36b6cf3fe36dce
Unnathi4 <119917958+Unnathi4@users.noreply.github.com>
2022-12-15 22:43:55 +0530

    Update test_handler.py

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -2,8 +2,23 @@ import json
 
 import unittest
 
-from hello_world import app
-
+def lambda_handler(event, context):
+    number1 = event['Number1']
+    number2 = event['Number2']
+    print(sum(number1,number2))
+    print(product(number1,number2))
+    print(difference(number1,number2))
+    print(quotient(number1,number2))
+    
+def sum(number1,number2):
+   return number1 + number2
+def product():
+    return number1 * number2
+def difference():
+    return abs(number1 - number2)
+def quotient():
+    return number1 / number2
+    
 class TestSum(unittest.TestCase):
 
     def test_add(self):
